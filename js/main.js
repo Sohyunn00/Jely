0062ba762cb9ff772aefb056e1ce31c3f95b60f5
sohyunkim <alice3405@naver.com>
2025-02-17 23:01:55 +0900

    [add] 햄버거 메뉴

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ const hamburgerMenu = document.querySelector(".hamburger_menu");
 
 function hamburgerOn(event) {
   event.preventDefault;
-  hamburgerMenu.style.display = "block";
+  if (hamburgerMenu.style.display === "none") {
+    hamburgerMenu.style.display = "block";
+  } else {
+    hamburgerMenu.style.display = "none";
+  }
 }
 
 hamburgerBtn.addEventListener("click", hamburgerOn);
+
+// 화면 크기가 변경될 때 메뉴 자동 닫기
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1024) {
+    hamburgerMenu.style.display = "none";
+  }
+});
