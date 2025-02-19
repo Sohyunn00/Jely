f74f74f3dc551510ab7a0e4c2d05a33f97583a81
sohyunkim <alice3405@naver.com>
2025-02-19 18:23:03 +0900

    [add] common폴더

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-const hamburgerBtn = document.querySelector(".ham_btn");
+// const hamburgerBtn = document.querySelector(".ham_btn");
 const hamburgerMenu = document.querySelector(".hamburger_menu");
 
-function hamburgerOn(event) {
-  event.preventDefault;
-  if (hamburgerMenu.style.display === "none") {
-    hamburgerMenu.style.display = "block";
-  } else {
-    hamburgerMenu.style.display = "none";
-  }
-}
+// function hamburgerOn(event) {
+//   event.preventDefault();
+//   if (hamburgerMenu.style.display === "none") {
+//     hamburgerMenu.style.display = "block";
+//   } else {
+//     hamburgerMenu.style.display = "none";
+//   }
+// }
 
-hamburgerBtn.addEventListener("click", hamburgerOn);
+// hamburgerBtn.addEventListener("click", hamburgerOn);
+
+$(function () {
+  $(".ham_btn").on("click", function (e) {
+    e.preventDefault();
+    $(".hamburger_menu").slideToggle(400);
+  });
+});
 
 // 화면 크기가 변경될 때 메뉴 자동 닫기
 window.addEventListener("resize", () => {
