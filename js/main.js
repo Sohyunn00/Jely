efff5152f17af7606257aeb4bf9a6ac3007ebb2e
sohyunkim <alice3405@naver.com>
2025-02-25 18:22:40 +0900

    [fix] 헤더 메뉴 수정

--- js/main.js
+++ js/main.js
@@ -1,43 +1,41 @@
-// const hamburgerBtn = document.querySelector(".ham_btn");
-const hamburgerMenu = document.querySelector(".hamburger_menu");
-
-// function hamburgerOn(event) {
-//   event.preventDefault();
-//   if (hamburgerMenu.style.display === "none") {
-//     hamburgerMenu.style.display = "block";
-//   } else {
-//     hamburgerMenu.style.display = "none";
-//   }
-// }
-
-// hamburgerBtn.addEventListener("click", hamburgerOn);
-
-// $(".ham_con > a").click(function (e) {
-//   e.preventDefault();
-//   $(".hamburger_menu").addClass("on");
-// });
-
-// $(".ham_con > a").click(function (e) {
-//   e.preventDefault();
-//   $(".hamburger_menu").removeClass("on");
-// });
-
 $(function () {
   $(".ham_btn").on("click", function (e) {
     e.preventDefault();
     $(".hamburger_menu").toggleClass("on");
+    $(".container .dimmed").toggleClass("on");
   });
-});
-$(function () {
-  $(".ham_con > a").on("click", function (e) {
+
+  $(".hamburger_menu > a").on("click", function (e) {
     e.preventDefault();
     $(".hamburger_menu").toggleClass("on");
+    $(".container .dimmed").toggleClass("on");
   });
-});
 
-// 화면 크기가 변경될 때 메뉴 자동 닫기
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 1024) {
-    hamburgerMenu.style.display = "none";
-  }
+  // 화면 크기가 변경될 때 메뉴 자동 닫기
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 1024) {
+      $(".hamburger_menu").removeClass("on");
+      $(".container .dimmed").removeClass("on");
+    }
+  });
+
+  $(".menu a").on({
+    mouseenter: function () {
+      let idx = $(this).index(); // 현재 a 태그의 index 가져오기
+      $(".submenu").eq(idx).addClass("on");
+    },
+    mouseleave: function () {
+      let idx = $(this).index();
+      $(".submenu").eq(idx).removeClass("on");
+    },
+  });
+
+  $(".submenu").on({
+    mouseenter: function () {
+      $(this).addClass("on");
+    },
+    mouseleave: function () {
+      $(this).removeClass("on");
+    },
+  });
 });
